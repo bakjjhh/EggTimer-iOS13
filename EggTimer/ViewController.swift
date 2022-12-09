81997da263e7a08a9a53aea3d8d5bf821b1d000b
junghun bak <wjdgnsi79@naver.com>
2022-12-09 23:22:36 +0900

    step1.5

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    let soft = 5
+    
+    let midium = 7
+    
+    let hard  = 12
+    
+    @IBAction func button(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        labelMonitor(timer: hardness)
+    }
+    
+    
+    func labelMonitor (timer : String) {
+        if timer == "Soft"{
+            print(soft)
+        }
+        if timer == "Medium" {
+            print(midium)
+        }
+        if timer == "Hard" {
+            print(hard)
+        }
+        
+        
+    }
+    
+    
+    
+    
 }
+
+
+
+
+
+
